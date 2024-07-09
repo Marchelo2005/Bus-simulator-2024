db14138c630e1d283242c45f90d3b35a2e27cddd
Marchelo2005 <marcelonacimba2005@gmail.com>
2024-07-08 22:39:51 -0500

    The MovimientoBus class was implemented

--- movementBus.py
+++ movementBus.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-class main():
+class main(object):
     pygame.init()
     size = (800, 600)
     screen = pygame.display.set_mode(size)
@@ -16,52 +16,48 @@ class main():
     personCont=0
     movPerson=1
     pauseMov=0
-def timeMovement():
-    clock= pygame.time.Clock()
-    clock.tick(100)
-def fund():
-    font=pygame.font.SysFont(None,20) 
-    x=(800*0.45)
-    y=(600*0.8)
-    #floortextureImg= pygame.image.load("grass.jpeg")
-    #stripImg= pygame.image.load("strip.jpg")
-    #yellowStripImg=pygame.image.load("yellow_strip.jpg")
+    def fund():
+     font=pygame.font.SysFont(None,20) 
+     x=(800*0.45)
+     y=(600*0.8)
+     #floortextureImg= pygame.image.load("grass.jpeg")
+     #stripImg= pygame.image.load("strip.jpg")
+     #yellowStripImg=pygame.image.load("yellow_strip.jpg")
    
-    #main.screen.blit(floortextureImg,(-150,0));
-    #main.screen.blit(floortextureImg,(700,0));
-    #main.screen.blit(yellowStripImg, (400,0))
-    #main.screen.blit(yellowStripImg, (400,100))
-    #main.screen.blit(yellowStripImg, (400,200))
-    #main.screen.blit(yellowStripImg, (400,300))
-    #main.screen.blit(yellowStripImg, (400,400))
-    #main.screen.blit(yellowStripImg, (400,500))
-    #main.screen.blit(yellowStripImg, (400,600))
-    #main.screen.blit(stripImg, (130,0))
-    #main.screen.blit(stripImg, (670,0))
+     #main.screen.blit(floortextureImg,(-150,0));
+     #main.screen.blit(floortextureImg,(700,0));
+     #main.screen.blit(yellowStripImg, (400,0))
+     #main.screen.blit(yellowStripImg, (400,100))
+     #main.screen.blit(yellowStripImg, (400,200))
+     #main.screen.blit(yellowStripImg, (400,300))
+     #main.screen.blit(yellowStripImg, (400,400))
+     #main.screen.blit(yellowStripImg, (400,500))
+     #main.screen.blit(yellowStripImg, (400,600))
+     #main.screen.blit(stripImg, (130,0))
+     #main.screen.blit(stripImg, (670,0))
      
       
-    moneyCollected=font.render("Money collected", True, (255,255,255))
-    maximumCapacity=font.render("Maximum capacity", True, (0,0,0))
-    passengers=font.render("Passengers", True, (0,0,0))
-    main.screen.blit(moneyCollected, (460,25))
-    main.screen.blit(maximumCapacity, (460,75))
-    main.screen.blit(passengers, (460,125)) 
- 
-def movementStrip():
-    floortextureImg= pygame.image.load("grass.png")
-    stripImg= pygame.image.load("strip.jpg")
-    yellowStripImg=pygame.image.load("yellow_strip.jpg")
-    stopImg=pygame.image.load("stop.png")
-    person=pygame.image.load("persprue.png")
+     moneyCollected=font.render("Money collected", True, (255,255,255))
+     maximumCapacity=font.render("Maximum capacity", True, (0,0,0))
+     passengers=font.render("Passengers", True, (0,0,0))
+     main.screen.blit(moneyCollected, (460,25))
+     main.screen.blit(maximumCapacity, (460,75))
+     main.screen.blit(passengers, (460,125)) 
+    def movementStrip():
+     floortextureImg= pygame.image.load("grass.png")
+     stripImg= pygame.image.load("strip.jpg")
+     yellowStripImg=pygame.image.load("yellow_strip.jpg")
+     stopImg=pygame.image.load("stop.png")
+     person=pygame.image.load("persprue.png")
    
-    if main.numberImg<=10:
-     recreoStopImg=pygame.image.load("shutdown"+repr(main.numberImg)+".png")
-    rel_y = main.numberOfYellowStripImg % 103
-    main.screen.blit(floortextureImg, (0, rel_y - 103))
-    main.screen.blit(floortextureImg, (700, rel_y - 103))
-    main.cont+=1
+     if main.numberImg<=10:
+      recreoStopImg=pygame.image.load("shutdown"+repr(main.numberImg)+".png")
+      rel_y = main.numberOfYellowStripImg % 103
+      main.screen.blit(floortextureImg, (0, rel_y - 103))
+      main.screen.blit(floortextureImg, (700, rel_y - 103))
+      main.cont+=1
   
-    if rel_y < 800:
+     if rel_y < 800:
        main.screen.blit(floortextureImg, (0, rel_y))
        main.screen.blit(floortextureImg, (700, rel_y))
        main.screen.blit(yellowStripImg,  (400, rel_y))
@@ -120,19 +116,14 @@ def movementStrip():
         main.stationStop=0
         main.numberImg+=1
         main.cont=0
-       
 
-
-def busPositioning(x,y):
-    busImg=pygame.image.load("staffBus.png")
-    busInsideImg=pygame.image.load("busInside.png")
-    main.screen.blit(busImg,(700,200))  
-    main.screen.blit(busInsideImg,(100,50)) 
-    
-
-
-def framework():
-    while True:
+    def busPositioning(x,y):
+     busImg=pygame.image.load("staffBus.png")
+     busInsideImg=pygame.image.load("busInside.png")
+     main.screen.blit(busImg,(700,200))  
+     main.screen.blit(busInsideImg,(100,50)) 
+    def framework():
+     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -144,6 +135,10 @@ def framework():
         movementStrip();
         busPositioning(0,0)   
         pygame.display.flip()
-            
-      
-framework();
+ 
+    framework();
+    pass
+
+
+
+
